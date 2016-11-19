f35e82ed601ac4e293809335a3f3794e6b4fe875
Milan Gruner <milangruner@gmail.com>
2016-11-19 16:30:25 +0100

    Fixes sentiment analysis

--- sentiments.py
+++ sentiments.py
@@ -1,14 +1,21 @@
 import urllib
 import requests
 import json
+import sys
 
 def analyzeSentiments(text):
 	url = "http://text-processing.com/api/sentiment/"
 	proxy = urllib.getproxies()
-	data_text = requests.get(url, proxies=proxy, params={'text': text}).text
-	data = json.load(data_text)
+	data_text = requests.post(url, proxies=proxy, data={'text': text}).text
 
 	# probabilities are in data["probability"][label]
 	# label can be "neg", "neutral" or "pos"
 
-	return data["label"]
+	try:
+		data = json.loads(data_text)
+		return data["label"]
+	except ValueError:
+		return None
+
+if __name__ == '__main__':
+	print analyzeSentiments(sys.argv[1])
