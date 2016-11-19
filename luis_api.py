edc6338f6367aaa9cfa1127a6f8270fbc238fc18
zelatyna <magda.sendal@gmail.com>
2016-11-19 16:13:24 +0100

    init commit

---
+++ luis_api.py
@@ -0,0 +1,31 @@
+import os, sys
+import requests
+from config import *
+
+luis_http_service ='https://api.projectoxford.ai/luis/v2.0/apps/ee569a80-115b-439f-a93d-3bfcdea54888?subscription-key=784bddb1f33f41adb474f784d12b3c0a&q=I%20feel%20sad&verbose=true'
+def callLUIS(queryText):
+	headers = {
+    # Request headers
+    'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
+	}
+	params={'q': queryText,
+		   'verbose' : 'True',
+		   'subscription-key' : SUBSCRIPTION_KEY}
+	base_url='https://api.projectoxford.ai/luis/v2.0/apps/{appId}'.format(appId=LUIS_APP_ID)
+	req= requests.get(base_url, params=params)
+	print req.text
+	return req.json()
+
+callLUIS("i feel sad")
+
+"""
+try:
+    conn = httplib.HTTPSConnection('api.projectoxford.ai')
+    conn.request("GET", "/luis/v1.0/prog/apps/{appId}/actions/{actionId}?%s" % params, "{body}", headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+	"""
---
+++ mozart_bot.py
@@ -0,0 +1,35 @@
+import sys
+import time
+import telepot
+from luis_api import *
+
+"""
+$ python2.7 skeleton.py <token>
+A skeleton for your telepot programs.
+"""
+
+def handle(msg):
+    #flavor = telepot.flavor(msg)
+
+    #summary = telepot.glance(msg, flavor=flavor)
+	content_type, chat_type, chat_id = telepot.glance(msg)
+	print(content_type, chat_type, chat_id)
+
+	if content_type == 'text':
+		#bot.sendMessage(chat_id, msg['from']['first_name'])
+		#print(msg['text'])
+		input = str(msg['text']).lower()
+		bot_resp= callLUIS(input)
+		bot.sendMessage(chat_id,'Hallo '+ msg['from']['first_name']+'!')
+    	bot.sendMessage(chat_id, bot_resp)
+
+
+TOKEN = sys.argv[1]  # 285502531:AAGPJIPn0aEmqVdXUvKptWTSN5x9RGIIph8
+
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+print 'Listening ...'
+
+# Keep the program running.
+while 1:
+	time.sleep(10)
