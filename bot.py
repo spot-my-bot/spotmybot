b2df48dcc017cfc757675943814d368f09537b15
zelatyna <magda.sendal@gmail.com>
2016-11-20 12:47:48 +0100

    change bot name

--- bot.py
+++ bot.py
@@ -10,20 +10,16 @@ import spotify
 import lyrics
 import textanalysis
 from luis_api import parseEntities
-BOT_NAME = "SpotMyBot"
-SENTIMENT_THRESHOLD = 0.2
-SPEED_THRESHOLD = 15
-MAX_FAILS = 5
+BOT_NAME = "Mozart"
+SENTIMENT_THRESHOLD = 0.3
 
 chats = {}
 
 def handle_message(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	response = ""
-	sentiment = None
-	faster_than = None
-	slower_than = None
-	task = messages["not_understood"]
+	sentiment = ""
+	task = "info"
 
 	if content_type == "text":
 		try:
@@ -31,37 +27,24 @@ def handle_message(msg):
 			print "Handling message: %s" % message
 
 			if chat_id not in chats:
-				chats[chat_id] = { "last_song": None }
+				chats[chat_id] = { "history": [] }
 				response = messages["introduction"]
-				task = "info"
-
-			if task != "info":
-				tags = textanalysis.get_speech_tags(message)
-				comparatives = tags["comparatives"]
-				if len(comparatives) > 0:
-					if "faster" in comparatives:
-						faster_than = chats[chat_id]['last_song']['tempo']
-						task = "improve_song"
-						response = messages["faster"]
-					if "slower" in comparatives:
-						slower_than = chats[chat_id]['last_song']['tempo']
-						task = "improve_song"
-						response = messages["slower"]
+				sentiment = "neutral"
+			else:
+				sentiment = sentiments.analyze_sentiment(message)
+				print sentiment
+				if sentiment is None:
+					response = messages["not_understood"]
 				else:
-					sentiment = sentiments.analyze_sentiment(message)
-					if sentiment is None:
-						response = messages["not_understood"]
+					if sentiment > 0.7:
+						response = messages["pos"]
+					elif sentiment < 0.3:
+						response = messages["neg"]
 					else:
-						if sentiment > 0.6:
-							response = messages["pos"]
-						elif sentiment < 0.2:
-							response = messages["neg"]
-						else:
-							response = messages["neutral"]
-							sentiment = None
-
-						task = "find_song"
-
+						response = messages["neutral"]
+					task = "find_song"
+			
+			chats[chat_id]['history'].append(message)
 			first_name = msg["from"]["first_name"]
 			emotion=parseEntities(message, 'Emotion')
 			
@@ -71,86 +54,35 @@ def handle_message(msg):
 
 			bot.sendMessage(chat_id, response)
 
-			if task == "info":
+			if task != "find_song":
 				return
 
-			search_terms = tags["nouns"]
-
-			if len(search_terms) < 1 or task is "improve_song":
-				if 'last_search_terms' in chats[chat_id]:
-					search_terms = chats[chat_id]['last_search_terms']
-				else:
-					bot.sendMessage(chat_id, messages["not_found"])
-					return
-			if sentiment is None and 'last_sentiment' in chats[chat_id]:
-				sentiment = chats[chat_id]['last_sentiment']
-
-			song = find_song(search_terms, sentiment, faster_than, slower_than)
-			if song:
-				chats[chat_id]['last_search_terms'] = search_terms
-				chats[chat_id]['last_song'] = song
-				chats[chat_id]['last_sentiment'] = sentiment
-				bot.sendMessage(chat_id, song["url"])
-			else:
-				song = find_song([search_terms[0]], sentiment, faster_than, slower_than)
-				if song:
-					bot.sendMessage(chat_id, song["url"])
-				else:
-					bot.sendMessage(chat_id, messages["not_found"])
+			link = find_song([message.split()[0]], sentiment)
+			if link:
+				bot.sendMessage(chat_id, link)
 		except:
 			print "Error!"
 			print traceback.format_exc()
 
-def find_song(search_words, sentiment=None, faster_than=None, slower_than=None):
-
-	print search_words, sentiment, faster_than, slower_than
-
-	spotify_data = None
-	try:
-		spotify_data = spotify.get_tracks(spotify_token, search_words, 20)
-	except:
-		print "No data from spotify! New key?"
-		return None
+def find_song(search_words, sentiment):
+	spotify_data = spotify.get_tracks(spotify_token, search_words, 20)
 
 	if len(spotify_data) < 1:
 		return None
 
 	selected_song = spotify_data[0]
-	fail_counter = 0
-
 	for song in spotify_data:
-		song_fits = True
+		text = lyrics.get_lyrics(song["artist"], song["name"])
+		song_sentiment = sentiments.analyze_sentiment(text)
 
-		print "Checking song %s - %s" % (song["artist"].encode('utf-8'), song["name"].encode('utf-8'))
+		if song_sentiment is None:
+			continue
 
-		if not faster_than is None:
-			tempo = song["tempo"]
-			if tempo <= faster_than:
-				song_fits = False
-			elif abs(faster_than - tempo) <= SPEED_THRESHOLD:
-				song_fits = False
-		elif not slower_than is None:
-			tempo = song["tempo"]
-			if tempo >= slower_than:
-				song_fits = False
-			elif abs(slower_than - tempo) <= SPEED_THRESHOLD:
-				song_fits = False
-		elif not sentiment is None:
-			text = lyrics.get_lyrics(song["artist"], song["name"])
-			song_sentiment = sentiments.analyze_sentiment(text)
-			if song_sentiment is None:
-				fail_counter += 1
-				if fail_counter > MAX_FAILS:
-					break
-				continue
-			if abs(song_sentiment - sentiment) >= SENTIMENT_THRESHOLD:
-				song_fits = False
-
-		if song_fits:
+		if abs(song_sentiment - sentiment) <= SENTIMENT_THRESHOLD:
 			selected_song = song
 			break
 
-	return selected_song
+	return selected_song["url"]
 
 def main():
 	global telegram_token, spotify_token, bot
